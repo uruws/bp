b205ac185a401fe40bb8f42ba3114d0fda407c8c
Jeremías <jeremias@talkingpts.org>
2021-05-03 20:27:12 -0300

    fix app build.sh

--- app/build.sh
+++ app/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 ROOT_URL=${ROOT_URL:-'http://localhost:3000/'}
-exec docker build $@ --rm -t uws/app:meteor-install-1.10.2
-	--build-arg ROOT_URL=${ROOT_URL}
+exec docker build $@ --rm -t uws/app:meteor-install-1.10.2 \
+	--build-arg ROOT_URL=${ROOT_URL} \
 	./app
