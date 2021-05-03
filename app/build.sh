3a9ab74f9515ae5ae040e66f6ddd9f1d77c17a9b
Jeremías <jeremias@talkingpts.org>
2021-05-03 20:29:30 -0300

    do not pass ROOT_URL at build time

--- app/build.sh
+++ app/build.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 set -eu
-ROOT_URL=${ROOT_URL:-'http://localhost:3000/'}
-exec docker build $@ --rm -t uws/app:meteor-install-1.10.2 \
-	--build-arg ROOT_URL=${ROOT_URL} \
-	./app
+exec docker build $@ --rm -t uws/app:meteor-install-1.10.2 ./app
