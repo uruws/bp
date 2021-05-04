2c2b2a0da9a080fce02194f0dd0fe05bae598518
Jeremías <jeremias@talkingpts.org>
2021-05-04 13:16:18 -0300

    try to fix phantomjs ssl issue

--- bundle/utils/meteor-build.sh
+++ bundle/utils/meteor-build.sh
@@ -4,6 +4,9 @@ set -eu
 # https://docs.meteor.com/environment-variables.html#METEOR-DISABLE-OPTIMISTIC-CACHING
 export METEOR_DISABLE_OPTIMISTIC_CACHING=1
 
+# https://github.com/kelaberetiv/TagUI/issues/787
+export OPENSSL_CONF=/tmp/fake-openssl.cnf
+
 cd ${HOME}/app
 
 exec meteor build /opt/app --directory --server-only
