5a0f01d4c67de20120bebb9d8ce6ba31414b5de8
Jeremías <jeremias@talkingpts.org>
2021-07-01 16:07:35 -0300

    fix docker build args

--- bundle/build.sh
+++ bundle/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 app_tag=${1:?'build tag?'}
-exec docker build $@ --rm -t uws/app:bundle-${app_tag} \
+exec docker build --rm -t uws/app:bundle-${app_tag} \
 	--build-arg APP_TAG=${app_tag} \
 	./bundle
--- deploy/build.sh
+++ deploy/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 app_tag=${1:?'build tag?'}
-exec docker build $@ --rm -t uws/app:deploy-${app_tag} \
+exec docker build --rm -t uws/app:deploy-${app_tag} \
 	--build-arg APP_TAG=${app_tag} \
 	./deploy
