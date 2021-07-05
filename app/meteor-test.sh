fb4defdd23f3ca2dda7a30e0ae5ef005a5817579
Jeremías <jeremias@talkingpts.org>
2021-07-05 16:49:15 -0300

    run test.sh script if exists

--- app/meteor-test.sh
+++ app/meteor-test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -eu
 cd ${HOME}/app
-exec meteor test --once --driver-package meteortesting:mocha \
-	--settings settings.json --port 3017
+if test -s ./test.sh; then
+	exec /bin/sh -eu ./test.sh
+fi
+echo "No tests to run!"
+exit 0
