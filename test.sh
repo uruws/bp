e6577c04624501f6655ce78fc910b8f25af5a61d
Jeremías <jeremias@talkingpts.org>
2021-08-04 16:03:02 -0300

    no interactive tests

--- test.sh
+++ test.sh
@@ -10,4 +10,4 @@ esac
 set -u
 app=${1:?'app name?'}
 app_tag=${2:?'build tag?'}
-exec docker run -it --rm -u uws uws/${app}:${app_tag} /usr/local/bin/meteor-test.sh
+exec docker run --rm -u uws uws/${app}:${app_tag} /usr/local/bin/meteor-test.sh
