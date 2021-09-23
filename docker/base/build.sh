a8500f2e8b9cb367445c5c86acc5c9be2e83feca
Jeremías <jeremias@talkingpts.org>
2021-09-23 17:12:29 -0300

    docker base 2109 upgrade

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -4,4 +4,10 @@ if test 'X0' = "X$(id -u)"; then
 	echo "do not run as root!" >&2
 	exit 1
 fi
-exec docker build $@ --rm -t uws/buildpack:base ./docker/base
+# base
+docker build $@ --rm -t uws/buildpack:base ./docker/base
+# base-2109
+docker build $@ --rm -t uws/buildpack:base-2109 \
+	-f docker/base/Dockerfile.2109 \
+	./docker/base
+exit 0
