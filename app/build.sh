213c9c84c44af6c9fdbe8092a04ed358dd2afce3
Jeremías <jeremias@talkingpts.org>
2021-09-23 19:36:29 -0300

    split beta build dir

--- app/build.sh
+++ app/build.sh
@@ -2,6 +2,9 @@
 set -eu
 app=${1:?'app name?'}
 app_tag=${2:?'build tag?'}
+if ! test -d ./${app}; then
+	echo "${app}: invalid app name" >&2
+	exit 9
+fi
 exec docker build --rm -t uws/${app}:${app_tag} \
-	--build-arg APP=${app} \
-	./app
+	--build-arg APP=${app} ./${app}
