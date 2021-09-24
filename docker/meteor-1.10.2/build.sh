d235c810ed68f9cc69992d6f3cec56f64be66ee8
Jeremías <jeremias@talkingpts.org>
2021-09-24 11:35:53 -0300

    meteor-1.10.2 2109 upgrade

--- docker/meteor-1.10.2/build.sh
+++ docker/meteor-1.10.2/build.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 set -eu
-exec docker build $@ --rm -t uws/meteor:1.10.2 ./docker/meteor-1.10.2
+# meteor-1.10.2
+docker build $@ --rm -t uws/meteor:1.10.2 ./docker/meteor-1.10.2
+# meteor-1.10.2-2109
+docker build $@ --rm -t uws/meteor:1.10.2-2109 \
+	-f ./docker/meteor-1.10.2/Dockerfile.2109 \
+	./docker/meteor-1.10.2
+exit 0
