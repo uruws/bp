36be9d79186728b46c8e20971ad3d73b312aabde
Jeremías <jeremias@talkingpts.org>
2021-11-26 15:45:00 -0300

    publish htmlcov report

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-exec make bootstrap
+set -eu
+
+make bootstrap
+
+covd=/srv/www/ssl/htmlcov
+if test -d "${covd}"; then
+	rm -rf ${covd}/buildpack
+	if test -d ./tmp/htmlcov; then
+		cp -r ./tmp/htmlcov ${covd}/buildpack
+	fi
+fi
+
+exit 0
