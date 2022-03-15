81af1d8b7c139a057a6191d9b6310f43721ca2af
Jeremías <jeremias@talkingpts.org>
2022-03-15 11:28:44 -0300

    deploy: push before tags

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ nice ionice make check
 if ! git remote | grep -q deploy; then
 	git remote add deploy uws@ops.uws.talkingpts.org:/srv/deploy/Buildpack.git
 fi
+git push deploy
 git push --tags deploy
 exit 0
