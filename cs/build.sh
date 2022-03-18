feae5cf14a0ca163586a3e3bcd5bb882f6cb597e
Jeremías <jeremias@talkingpts.org>
2022-03-18 13:08:30 -0300

    cs: 2203 docker image

--- cs/build.sh
+++ cs/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -eu
 app_tag=${1:?'build tag?'}
-exec docker build --rm -t uws/crowdsourcing:${app_tag} ./cs
+# crowdsourcing
+docker build --rm -t uws/crowdsourcing:${app_tag}
+	-f cs/Dockerfile ./cs
+exit 0
