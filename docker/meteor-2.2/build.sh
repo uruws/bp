cbb646c7d19c6aa32e51ab6253b14892a5b060ea
Jeremías <jeremias@talkingpts.org>
2022-03-18 13:03:22 -0300

    docker/meteor-2.2: 2203 upgrade

--- docker/meteor-2.2/build.sh
+++ docker/meteor-2.2/build.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 set -eu
-exec docker build "$@" --rm -t uws/meteor:2.2 ./docker/meteor-2.2
+# 2.2
+docker build "$@" --rm -t uws/meteor:2.2 \
+	-f docker/meteor-2.2/Dockerfile \
+	./docker/meteor-2.2
+# 2.2-2203
+docker build "$@" --rm -t uws/meteor:2.2-2203 \
+	-f docker/meteor-2.2/Dockerfile.2203 \
+	./docker/meteor-2.2
+exit 0
