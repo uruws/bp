6fbb416dc2176a75b9dd4e3ab47a4b6d0c32a864
Jeremías <jeremias@talkingpts.org>
2022-03-18 13:13:16 -0300

    docker/meteor-1.10.2: 2203 upgrade

--- docker/meteor-1.10.2/build.sh
+++ docker/meteor-1.10.2/build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -eu
-# meteor-1.10.2
+# 1.10.2
 docker build "$@" --rm -t uws/meteor:1.10.2 ./docker/meteor-1.10.2
-# meteor-1.10.2-2109
+# 1.10.2-2109
 docker build "$@" --rm -t uws/meteor:1.10.2-2109 \
 	-f ./docker/meteor-1.10.2/Dockerfile.2109 \
+	./docker/meteor-1.10.2
+# 1.10.2-2203
+docker build "$@" --rm -t uws/meteor:1.10.2-2203 \
+	-f ./docker/meteor-1.10.2/Dockerfile.2203 \
 	./docker/meteor-1.10.2
 exit 0
