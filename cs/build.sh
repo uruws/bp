b6f62a5f2aa686ba510123d1768e906d8b2180f8
Jeremías <jeremias@talkingpts.org>
2022-03-22 15:14:56 -0300

    fix typo

--- cs/build.sh
+++ cs/build.sh
@@ -2,7 +2,7 @@
 set -eu
 app_tag=${1:?'build tag?'}
 # crowdsourcing
-docker build --rm -t uws/crowdsourcing:${app_tag}
+docker build --rm -t uws/crowdsourcing:${app_tag} \
 	-f cs/Dockerfile.2203 \
 	./cs
 exit 0
