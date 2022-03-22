684f46c02ea7acc6c6bc28612fac3b6aec9c7c4b
Jeremías <jeremias@talkingpts.org>
2022-03-22 12:42:21 -0300

    meteor-devel 2203 upgrade

--- docker/meteor-devel/build.sh
+++ docker/meteor-devel/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 set -eu
-exec docker build "$@" --rm -t uws/meteor:devel ./docker/meteor-devel
+exec docker build "$@" --rm -t uws/meteor:devel \
+	-f docker/meteor-devel/Dockerfile.2203 \
+	./docker/meteor-devel
