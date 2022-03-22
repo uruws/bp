4c9bf885e2293b4fc1abf99ca47c65366448967f
Jeremías <jeremias@talkingpts.org>
2022-03-22 14:40:43 -0300

    infra-ui: 2203 upgrade

--- infra-ui/build.sh
+++ infra-ui/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -eu
 app_tag=${1:?'build tag?'}
-exec docker build --rm -t uws/infra-ui:${app_tag} ./infra-ui
+exec docker build --rm -t uws/infra-ui:${app_tag} \
+	-f infra-ui/Dockerfile.2203 \
+	./infra-ui
