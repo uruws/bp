ca15ec6dc1bc5ddecdff6d0d43e849c35bc7f907
Jeremías <jeremias@talkingpts.org>
2022-07-07 12:50:33 -0300

    docker: meteor-2.2 old images cleanup

--- docker/meteor-2.2/build.sh
+++ docker/meteor-2.2/build.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 set -eu
-# 2.2
-#docker build "$@" --rm -t uws/meteor:2.2 \
-#	-f docker/meteor-2.2/Dockerfile \
-#	./docker/meteor-2.2
-docker rmi uws/meteor:2.2 || true
 # 2.2-2203
 docker build "$@" --rm -t uws/meteor:2.2-2203 \
 	-f docker/meteor-2.2/Dockerfile.2203 \
