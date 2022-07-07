5a0ad625d3af85be322d2e723829be95485d22ae
Jeremías <jeremias@talkingpts.org>
2022-07-07 12:41:38 -0300

    docker: remove cleanup of old base images

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -4,14 +4,6 @@ if test 'X0' = "X$(id -u)"; then
 	echo "do not run as root!" >&2
 	exit 1
 fi
-# base
-#docker build "$@" --rm -t uws/buildpack:base ./docker/base
-docker rmi uws/buildpack:base || true
-# base-2109
-#docker build "$@" --rm -t uws/buildpack:base-2109 \
-#	-f docker/base/Dockerfile.2109 \
-#	./docker/base
-docker rmi uws/buildpack:base-2109 || true
 # base-2203
 docker build "$@" --rm -t uws/buildpack:base-2203 \
 	-f docker/base/Dockerfile.2203 \
