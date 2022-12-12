20e2bc31afaedec115fa25846d3ea592774a9180
Jeremías <jeremias@talkingpts.org>
2022-12-12 11:59:00 -0300

    upgrade: docker/meteor-devel 2211

--- docker/meteor-devel/build.sh
+++ docker/meteor-devel/build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -eu
-exec docker build "$@" --rm -t uws/meteor:devel \
+# remove old versions
+docker rmi uws/meteor:devel-2109 || true
+# uws/meteor:devel-2203
+docker build --rm -t uws/meteor:devel-2203 \
 	-f docker/meteor-devel/Dockerfile.2203 \
 	./docker/meteor-devel
+# uws/meteor:devel-2211
+docker build --rm -t uws/meteor:devel-2211 \
+	-f docker/meteor-devel/Dockerfile.2211 \
+	./docker/meteor-devel
+exit 0
--- docker/meteor-devel/devel.sh
+++ docker/meteor-devel/devel.sh
@@ -2,4 +2,4 @@
 exec docker run -it --rm --name uws-meteor-devel \
 	--hostname meteor-devel.uws.local -u uws \
 	-v ${PWD}/docker/meteor-devel/check:/home/uws/check \
-	uws/meteor:devel "$@"
+	uws/meteor:devel-2211 "$@"
