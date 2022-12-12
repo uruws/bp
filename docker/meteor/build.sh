45a150c44c8682efb0611ba9d93791b752c365a9
Jeremías <jeremias@talkingpts.org>
2022-12-12 11:54:27 -0300

    upgrade: docker/meteor 2211

--- docker/meteor/build.sh
+++ docker/meteor/build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -eu
-exec docker build "$@" --rm -t uws/meteor-2203 \
+# remove old versions
+docker rmi uws/meteor-2109 || true
+# uws/meteor-2203
+docker build --rm -t uws/meteor-2203 \
 	-f docker/meteor/Dockerfile.2203 \
 	./docker/meteor
+# uws/meteor-2211
+docker build --rm -t uws/meteor-2211 \
+	-f docker/meteor/Dockerfile.2211 \
+	./docker/meteor
+exit 0
