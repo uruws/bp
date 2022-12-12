3dc1c3060e9039132829a679eee1c4c701832db8
Jeremías <jeremias@talkingpts.org>
2022-12-12 12:34:37 -0300

    upgrades: meteor-vanilla 2211

--- meteor-vanilla/build.sh
+++ meteor-vanilla/build.sh
@@ -2,5 +2,5 @@
 set -eu
 app_tag=${1:?'build tag?'}
 exec docker build --rm -t uws/meteor-vanilla:${app_tag} \
-	-f meteor-vanilla/Dockerfile.2203 \
+	-f meteor-vanilla/Dockerfile \
 	./meteor-vanilla
