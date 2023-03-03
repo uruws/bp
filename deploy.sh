c7c9001ce7fb50682ec2282497cd7bb59cea1a78
Jeremías <jeremias@talkingpts.org>
2023-03-03 15:35:07 -0300

    deploy push to origin first

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ nice ionice make check
 if ! git remote | grep -q deploy; then
 	git remote add deploy uws@ops.uws.talkingpts.org:/srv/deploy/Buildpack.git
 fi
+git push
+git push --tags
 git push deploy
 git push --tags deploy
 exit 0
