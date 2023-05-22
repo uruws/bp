537edcdc37722da95018c48eaf998ee60b062398
Jeremías <jeremias@talkingpts.org>
2023-05-22 16:40:16 -0300

    docker: apps upgrades 2305

--- docker/upgrades-all.sh
+++ docker/upgrades-all.sh
@@ -13,15 +13,15 @@ ${upgrd} -t uws/buildpack:base -U docker/base
 ${upgrd} -t uws/buildpack:base
 
 # uws/buildpack:devel
-${upgrd} -t uws/buildpack:devel -U docker/devel -s uws/python
+${upgrd} -t uws/buildpack:devel -s uws/python -U docker/devel
 ${upgrd} -t uws/buildpack:devel
 
 # uws/meteor
-${upgrd} -t uws/meteor -U docker/meteor -s uws/buildpack:base
+${upgrd} -t uws/meteor -s uws/buildpack:base -U docker/meteor
 ${upgrd} -t uws/meteor
 
 # uws/meteor:devel
-${upgrd} -t uws/meteor:devel -U docker/meteor-devel -s uws/meteor
+${upgrd} -t uws/meteor:devel -s uws/meteor -U docker/meteor-devel
 ${upgrd} -t uws/meteor:devel
 
 exit 0
