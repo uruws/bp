58245dbff75b955a53a5a329e92fc7d7ddcf1992
Jeremías <jeremias@talkingpts.org>
2023-07-28 17:09:54 -0300

    osx: install Docker.app for current user only

--- osx/docker/install.sh
+++ osx/docker/install.sh
@@ -14,7 +14,7 @@ rm -vf /var/tmp/Docker.dmg
 curl -o /var/tmp/Docker.dmg https://desktop.docker.com/mac/main/arm64/Docker.dmg
 
 sudo hdiutil attach /var/tmp/Docker.dmg
-sudo /Volumes/Docker/Docker.app/Contents/MacOS/install
+sudo /Volumes/Docker/Docker.app/Contents/MacOS/install --user="${USER}"
 sudo hdiutil detach /Volumes/Docker
 
 exit 0
