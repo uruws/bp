9f885dfc052663283f1907558d96acb06830f311
Jeremías <jeremias@talkingpts.org>
2023-07-28 16:59:27 -0300

    osx: remove Docker.app after uninstall

--- osx/docker/uninstall.sh
+++ osx/docker/uninstall.sh
@@ -5,7 +5,9 @@ brew uninstall docker
 brew uninstall homebrew/cask/docker
 
 if test -x /Applications/Docker.app/Contents/MacOS/uninstall; then
-	sudo /Applications/Docker.app/Contents/MacOS/uninstall
+	sudo /Applications/Docker.app/Contents/MacOS/uninstall && (
+		sudo /Applications/Docker.app/Contents/MacOS/uninstall
+	)
 fi
 
 exit 0
