78532afcf2f26b3eb96b64d20df8e4df58bfd26d
Jeremías <jeremias@talkingpts.org>
2023-07-31 16:59:21 -0300

    osx: install check OS arch

--- osx/docker/install.sh
+++ osx/docker/install.sh
@@ -4,13 +4,8 @@ set -xeu
 
 sudo softwareupdate --install-rosetta
 
-brew update
-brew upgrade
-
-#brew install docker
-
 rm -vf /var/tmp/Docker.dmg
-curl -o /var/tmp/Docker.dmg https://desktop.docker.com/mac/main/arm64/Docker.dmg
+curl -o /var/tmp/Docker.dmg "https://desktop.docker.com/mac/main/$(uname -m)/Docker.dmg"
 
 sudo hdiutil attach /var/tmp/Docker.dmg
 sudo /Volumes/Docker/Docker.app/Contents/MacOS/install --accept-license --user="${USER}"
