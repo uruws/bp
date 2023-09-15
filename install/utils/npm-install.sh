8e4398db2e8a1e8db9edb4655658feb9e16cd5b6
Jeremías <jeremias@talkingpts.org>
2023-09-15 12:54:30 -0300

    fix gyp python exec issue

--- install/utils/npm-install.sh
+++ install/utils/npm-install.sh
@@ -16,4 +16,9 @@ npm   --version
 which npx
 npx   --version
 
+# https://github.com/nodejs/node-gyp#on-unix
+export PYTHON=/usr/bin/python3
+export NODE_GYP_FORCE_PYTHON=/usr/bin/python3
+export npm_config_python=/usr/bin/python3
+
 exec npm install --no-audit
