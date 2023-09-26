41d09454518bf43fad85ce279b45d39f2081917e
Jeremías <jeremias@talkingpts.org>
2023-09-26 13:56:29 -0300

    install: revert f8aa2232

--- bundle/utils/meteor-build.sh
+++ bundle/utils/meteor-build.sh
@@ -10,11 +10,6 @@ export OPENSSL_CONF=/var/tmp/fake-openssl.cnf
 
 cd ${HOME}/${app}
 
-# https://github.com/nodejs/node-gyp#on-unix
-export PYTHON=/usr/bin/python3
-export NODE_GYP_FORCE_PYTHON=/usr/bin/python3
-export npm_config_python=/usr/bin/python3
-
 # From heroku build log:
 #   /tmp/codon/tmp/cache/meteor/.meteor/meteor build --platforms=web.browser --server https://dev.talkingpts.org/ --directory /tmp/codon/tmp/buildpacks/12684dcdaf0c8e69ace478395557e8d1c737cf0c/build-fObn
 
--- install/utils/npm-install.sh
+++ install/utils/npm-install.sh
@@ -16,11 +16,4 @@ npm   --version
 which npx
 npx   --version
 
-# https://github.com/nodejs/node-gyp#on-unix
-export PYTHON=/usr/bin/python3
-export NODE_GYP_FORCE_PYTHON=/usr/bin/python3
-export npm_config_python=/usr/bin/python3
-
-npm install -g node-gyp
-
 exec npm install --no-audit
